f10d43f10ebbc4f9898aa6cb8f820478edb29241
Mat Bishop <matthew.g.bishop@accenture.com>
2017-10-03 21:55:43 +0100

    better test

--- test.sh
+++ test.sh
@@ -6,3 +6,10 @@ read -p 'Bus: ' bus
 read -p 'Device: ' device
 
 lsusb -s $bus:$device -v | egrep "Width|Height"
+
+echo "=================="
+echo "=================="
+
+v4l2-ctl --list-formats-ext
+
+echo "=================="
